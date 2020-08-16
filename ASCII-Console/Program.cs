bbc668dfeab5e7881a6aa82e64c9a38a7555acf4
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-08-17 04:37:54 +0700

    Optimize Code(?), Update Bass Library

--- ASCII-Console/Program.cs
+++ ASCII-Console/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+using System.Collections;
 using System.IO;
+using System.Threading;
 using Un4seen.Bass;
 
-
-namespace Bad_Apple
+namespace ASCII
 {
     class Program
     {
@@ -16,42 +13,73 @@ namespace Bad_Apple
             Un4seen.Bass.BassNet.Registration("anamnafiul99@gmail.com", "2X3141322152222");
             Console.Title = "ASCII-Console";
             Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
-            
-            string dir = Directory.GetCurrentDirectory();
-            int framecount = Directory.GetFiles(dir+"/files/frames").Length; //ugot
-            int play = Bass.BASS_StreamCreateFile(dir+"/files/audio/ugot.mp3", 0, 0, BASSFlag.BASS_DEFAULT); //ugot
-            //int framecount = Directory.GetFiles(dir + "/files/frames_1").Length; //bad-apple
-            //int play = Bass.BASS_StreamCreateFile(dir + "/files/audio/bad-apple.mp3", 0, 0, BASSFlag.BASS_DEFAULT); //bad-apple
-
-            Console.SetWindowSize(166, 54); //ugot
-            //Console.SetWindowSize(124, 54); //bad-apple
-            Console.WriteLine("Created By NaMLiM");
-            Thread.Sleep(2000);
+            string Dir = Directory.GetCurrentDirectory();
+            int Error = 1, Input = 0, Framecount = 0, Play = 0;
+            var Contents = new ArrayList();
+            while (Error != 0)
+            {
+                Console.WriteLine("Created By NaMLiM");
+                Console.WriteLine("Please select the ASCII Sequence:");
+                Console.WriteLine("1. U Got That");
+                Console.WriteLine("2. Bad Apple");
+                try
+                {
+                    Input = Convert.ToInt32(Console.ReadLine());
+                    Error = 0;
+                }
+                catch (System.FormatException e)
+                {
+                    Error = 1;
+                    Console.WriteLine(e);
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+                if (Input == 1) //U-Got-That
+                {
+                    Framecount = Directory.GetFiles(Dir + "/files/frames").Length;
+                    Play = Bass.BASS_StreamCreateFile(Dir + "/files/audio/ugot.mp3", 0, 0, BASSFlag.BASS_DEFAULT);
+                    Console.WriteLine("Please Wait...");
+                    for (int Frame = 1; Frame <= Framecount; Frame++)
+                    {
+                        Contents.Add(File.ReadAllText(Dir + "/files/frames/ASCII-ugot-" + (Frame) + ".txt"));
+                    }
+                    Console.SetWindowSize(166, 54);
+                }
+                else if (Input == 2) //Bad-Apple
+                {
+                    Framecount = Directory.GetFiles(Dir + "/files/frames_1").Length;
+                    Play = Bass.BASS_StreamCreateFile(Dir + "/files/audio/bad-apple.mp3", 0, 0, BASSFlag.BASS_DEFAULT);
+                    Console.WriteLine("Please Wait...");
+                    for (int Frame = 1; Frame <= Framecount; Frame++)
+                    {
+                        Contents.Add(File.ReadAllText(Dir + "/files/frames_1/ASCII-bad-apple-" + (Frame) + ".txt"));
+                    }
+                    Console.SetWindowSize(125, 54);
+                }
+                else
+                {
+                    Error = 1;
+                    Console.WriteLine("Input Salah!");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+            }
             Console.WriteLine("Press Any Key To Start..");
             Console.ReadKey();
             Console.Clear();
-
-            for(int frame = 1; frame <= framecount; frame++)
+            Bass.BASS_ChannelPlay(Play, false);
+            for (int Frame = 0; Frame <= Framecount; Frame++)
             {
-                if(frame == 1)
-                {
-                    Bass.BASS_ChannelPlay(play, false);
-                }
-                string contents = File.ReadAllText(dir+ "/files/frames/ASCII-ugot-"+(frame)+".txt"); //ugot
-                //string contents = File.ReadAllText(dir + "/files/frames_1/ASCII-bad-apple-" + (frame) + ".txt"); //bad-apple
-                Console.Write(contents);
+                Console.Write(Contents[Frame]);
                 Console.SetCursorPosition(0, 54);
-                if (frame == framecount)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Created By NaMLiM");
-                    Thread.Sleep(2000);
-                    Console.WriteLine("Press Any Key To Exit");
-                    Console.ReadKey();
-                    return;
-                }
-                Thread.Sleep(18);
+                Thread.Sleep(28);
             }
+            Console.Clear();
+            Console.WriteLine("Created By NaMLiM");
+            Thread.Sleep(2000);
+            Console.WriteLine("Press Any Key To Exit");
+            Console.ReadKey();
+            return;
         }
     }
 }
